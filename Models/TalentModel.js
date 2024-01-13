e737df416592b3049985e8dad14a843ce7e1ed7e
DawitSishu <dawitalemayhu55@gmail.com>
2024-01-13 13:26:59 -0500

    talent hub

---
+++ Models/TalentModel.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const Users = require("./Users");
+
+const ApplicantSchema = mongoose.Schema({
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Users",
+    required: [true, "must specify a user"],
+  },
+  firstName: {
+    type: String,
+    required: [true, "must specify a first name"],
+  },
+  lastName: {
+    type: String,
+    required: [true, "must specify a last name"],
+  },
+  city: {
+    type: String,
+    required: [true, "must specify a city"],
+  },
+  phoneNumber: {
+    type: String,
+    required: [true, "must specify a phone number"],
+  },
+  languages: {
+    type: [String],
+    default: [],
+  },
+  skills: {
+    type: [String],
+    default: [],
+  },
+});
+
+module.exports = mongoose.model("Applicants", ApplicantSchema);
+u;
